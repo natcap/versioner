c7b4a386cf20dfa7c5fcec7e29d53bfdf93060e9
James Douglass <jdouglass@stanford.edu>
2015-07-07 16:28:39 -0700

    Correcting the get_version function we'll use for getting vesions

--- natcap/versioner/__init__.py
+++ natcap/versioner/__init__.py
@@ -1,12 +1,23 @@
 import pkg_resources
-import versioning
 
 def get_version(package, root='.'):
+    """
+    Get the version string for the target package.
+
+    If `package` is not available for import, check the root for git or hg.
+
+    Parameters:
+        package (string): The package name to check for (e.g. 'natcap.invest')
+        root='.' (string): The path to the directory to check for a DVCS repository.
+
+    Returns:
+        A DVCS-aware versioning string.
+    """
     try:
         return pkg_resources.require(package)[0].version
     except pkg_resources.DistributionNotFound:
         pass
 
+    import versioning
     return versioning.get_pep440(branch=False)
-
 
