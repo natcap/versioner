20f6b0287fccb377af3c0576f454434c1ecfef20
James Douglass <jdouglass@stanford.edu>
2015-07-07 15:09:08 -0700

    Updating test file

--- test.sh
+++ test.sh
@@ -1,6 +1,15 @@
-virtualenv test_env --clear
-source test_env/bin/activate
-pip install pyyaml
-python setup.py install
-python setup.py --version
-python -c "import natcap.versioner; print 'FOUND', natcap.versioner.get_version()"
+if [ "$1" = "build" ]
+then
+    virtualenv test_env --clear --system-site-packages
+    source test_env/bin/activate
+    python setup.py install
+    pushd ../invest
+    python setup.py install
+    popd
+else
+    source test_env/bin/activate
+    python setup.py install
+fi
+
+python -c "import natcap.versioner; print 'FOUND', natcap.versioner.get_version('natcap.invest')"
+python -c "import natcap.invest; print 'INVEST', natcap.invest.__version__"
