9e4852e35c104841acc32677c17a15ef8173b38a
James Douglass <jdouglass@stanford.edu>
2015-07-07 17:38:04 -0700

    Adding 0.1.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'natcap',
         'natcap.versioner',
     ],
-    version='0.1.0',
+    version='0.1.1',
     license=LICENSE,
     zip_safe=True,
     keywords='hg mercurial git versioning natcap',
