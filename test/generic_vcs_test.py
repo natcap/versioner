52481e44f4ab6e3fe90c1b6c9efcaa46fabd864c
James Douglass <jdouglass@stanford.edu>
2016-11-06 13:37:55 -0800

    Adding docstrings to generic_vcs_test and git_test.

--- test/generic_vcs_test.py
+++ test/generic_vcs_test.py
@@ -3,48 +3,57 @@ import unittest
 
 class GenericVCSTest(unittest.TestCase):
     def test_is_archive(self):
+        """Versioner: test when a repo is not an archive."""
         from natcap.versioner import versioning
         repo = versioning.VCSQuerier('.')
         self.assertFalse(repo.is_archive)
 
     def test_tag_distance(self):
+        """Versioner: check NotImplementedError on tag_distance."""
         from natcap.versioner import versioning
         repo = versioning.VCSQuerier('.')
         with self.assertRaises(NotImplementedError):
             repo.tag_distance
 
     def test_build_id(self):
+        """Versioner: check NotImplementedError on build_id."""
         from natcap.versioner import versioning
         repo = versioning.VCSQuerier('.')
         with self.assertRaises(NotImplementedError):
             repo.build_id
 
     def test_latest_tag(self):
+        """Versioner: check NotImplementedError on latest_tag."""
         from natcap.versioner import versioning
         repo = versioning.VCSQuerier('.')
         with self.assertRaises(NotImplementedError):
             repo.latest_tag
 
     def test_branch(self):
+        """Versioner: check NotImplementedError on branch."""
         from natcap.versioner import versioning
         repo = versioning.VCSQuerier('.')
         with self.assertRaises(NotImplementedError):
             repo.branch
 
     def test_vcs_version_error_raise(self):
+        """Versioner: check error raised as expected when requested."""
         import natcap.versioner
         with self.assertRaises(natcap.versioner.VersionNotFound):
             natcap.versioner.vcs_version(
                 '/', on_error=natcap.versioner.ERROR_RAISE)
 
     def test_vcs_version_error_return(self):
+        """Versioner: check error returned as expected when requested."""
         import natcap.versioner
         # This will return an error string instead of a version, but that's ok
         # because we told it to return via ERROR_RETURN
-        natcap.versioner.vcs_version(
+        error_string = natcap.versioner.vcs_version(
             '/', on_error=natcap.versioner.ERROR_RETURN)
+        self.assertTrue(len(error_string) != 0)
 
     def test_node_version_error_raise(self):
+        """Versioner: check NotImplementedError on node."""
         from natcap.versioner import versioning
         repo = versioning.VCSQuerier('.')
         with self.assertRaises(NotImplementedError):
--- test/git_test.py
+++ test/git_test.py
@@ -7,6 +7,19 @@ import re
 
 
 def call_git(command, repo_dir):
+    """
+    Make a call to the shell via ``subprocess.check_call``.
+
+    Parameters:
+        command (string): The command to issue.
+        repo_dir (string): The directory where the git repo resides.
+
+    Returns:
+        ``None``.
+
+    Raises:
+        subprocess.CalledProcessError: ``command`` exited with nonzero code.
+    """
     # The subprocess docs warn that using subprocess.PIPE may result in
     # deadlock when used with subprocess.call, but it seems to work ok
     # so far!
@@ -17,13 +30,30 @@ def call_git(command, repo_dir):
 
 class GitTest(unittest.TestCase):
     def setUp(self):
+        """Set up ``self.repo_uri`` as a new temp folder."""
         self.repo_uri = tempfile.mkdtemp()
 
     def tearDown(self):
+        """Remove the temp folder self.repo_uri."""
         shutil.rmtree(self.repo_uri)
 
     def _set_up_sample_repo(self, tag=True):
-        """Create a sample repo with four commits and a tag.
+        """Create a sample repo with four commits and (optionally) a tag.
+
+        ``self.repo_uri`` is used as the repository location.
+
+        All changes are made to the file ``<self.repo_uri>/scratchfile``
+
+        A few git configuration options are assumed for ease of testing on CI
+        machines, where the currently executing user may not have this
+        configured:
+
+            * ``user.name`` is set to ``Example Name``
+            * ``user.email`` is set to ``name@example.com``
+
+        Parameters:
+            tag=True (bool): whether to include a tag in the repo.  If
+                ``True``, the 4th commit will be tagged with ``0.1``.
 
         Returns:
             None"""
@@ -67,26 +97,32 @@ class GitTest(unittest.TestCase):
         return versioning.GitRepo(self.repo_uri)
 
     def test_tag_distance(self):
+        """Versioner - Git: Check tag distance."""
         repo = self._set_up_sample_repo()
         self.assertEqual(repo.tag_distance, 1)
 
     def test_branch(self):
+        """Versioner - Git: Check branch name."""
         repo = self._set_up_sample_repo()
         self.assertEqual(repo.branch, 'master')
 
     def test_latest_tag(self):
+        """Versioner - Git: Check latest tag."""
         repo = self._set_up_sample_repo()
         self.assertEqual(repo.latest_tag, '0.1')
 
     def test_node(self):
+        """Versioner - Git: check latest node ID."""
         repo = self._set_up_sample_repo()
         self.assertEqual(len(repo.node), 8)
 
     def test_is_archive(self):
+        """Versioner - Git: check whether repo is an archive."""
         repo = self._set_up_sample_repo()
         self.assertEqual(repo.is_archive, False)
 
     def test_build_id(self):
+        """Versioner - Git: check build ID matches known regex."""
         repo = self._set_up_sample_repo()
         build_id = repo.build_id
         matches = re.findall('1:0\.1 \[[0-9a-f]{8,12}\]', build_id)
@@ -94,6 +130,7 @@ class GitTest(unittest.TestCase):
         self.assertEqual(build_id, matches[0])
 
     def test_dev_id_default(self):
+        """Versioner - Git: check dev build ID matches known regex."""
         repo = self._set_up_sample_repo()
         dev_id = repo.build_dev_id()
         matches = re.findall('dev1:0\.1 \[[0-9a-f]{8,12}\]', dev_id)
@@ -101,16 +138,19 @@ class GitTest(unittest.TestCase):
         self.assertEqual(dev_id, matches[0])
 
     def test_dev_id_provided(self):
+        """Versioner - Git: check dev build ID of known string."""
         from natcap.versioner import versioning
         repo = versioning.HgRepo('.')
         self.assertEqual(repo.build_dev_id('foo'), 'devfoo')
 
     def test_release_version_at_tag(self):
+        """Versioner - Git: check version at tag."""
         repo = self._set_up_sample_repo()
         call_git('git checkout 0.1', self.repo_uri)
         self.assertEqual(repo.version, '0.1')
 
     def test_release_version_not_at_tag(self):
+        """Versioner - Git: check version not at tag."""
         repo = self._set_up_sample_repo()
         dev_id = repo.version
         matches = re.findall('dev1:0\.1 \[[0-9a-f]{8,12}\]', dev_id)
@@ -118,6 +158,7 @@ class GitTest(unittest.TestCase):
         self.assertEqual(dev_id, matches[0])
 
     def test_vcs_version(self):
+        """Versioner - Git: check PEP440 version from VCS."""
         import natcap.versioner
         repo = self._set_up_sample_repo()
         call_git('git checkout 0.1', self.repo_uri)
@@ -125,6 +166,7 @@ class GitTest(unittest.TestCase):
         self.assertEqual(version, repo.pep440(branch=False))
 
     def test_vcs_version_no_tag(self):
+        """Versioner - Git: check PEP440 version from VCS without a tag."""
         import natcap.versioner
         self._set_up_sample_repo(tag=False)
         version = natcap.versioner.vcs_version(self.repo_uri)
@@ -133,6 +175,7 @@ class GitTest(unittest.TestCase):
         self.assertEqual(version, matches[0])
 
     def test_git_no_branches(self):
+        """Versioner - Git: check error raised when no branches exist."""
         from natcap.versioner import versioning
 
         # A newly initialized repo has no commits.  A repo with no commits has
@@ -145,6 +188,7 @@ class GitTest(unittest.TestCase):
             repo.branch
 
     def test_git_vcs_version(self):
+        """Versioner - Git: check fallback to SCM."""
         import natcap.versioner
 
         self._set_up_sample_repo()
